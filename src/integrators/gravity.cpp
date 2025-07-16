b3799beec85e1e38eca28616dc6165e7df23b381
Shin-Rong Tsai <turquoisea.tsai@gmail.com>
2025-07-15 23:07:46 -0500

    Set up gravity integrator and make sure it produces the same result as path integrator without bsdf for now.

--- src/integrators/gravity.cpp
+++ src/integrators/gravity.cpp
@@ -1,3 +1,7 @@
+#include <mitsuba/core/ray.h>
+#include <mitsuba/core/properties.h>
+#include <mitsuba/render/bsdf.h>
+#include <mitsuba/render/emitter.h>
 #include <mitsuba/render/integrator.h>
 #include <mitsuba/render/records.h>
 
@@ -6,25 +10,100 @@ NAMESPACE_BEGIN(mitsuba)
 template <typename Float, typename Spectrum>
 class GravityIntegrator final : public SamplingIntegrator<Float, Spectrum> {
 public:
-    MI_IMPORT_BASE(SamplingIntegrator)
-    MI_IMPORT_TYPES(Scene, Sampler, Medium)
+    MI_IMPORT_BASE(SamplingIntegrator, m_hide_emitters)
+    MI_IMPORT_TYPES(Scene, Sampler, Medium, Emitter, EmitterPtr, BSDF, BSDFPtr)
 
     GravityIntegrator(const Properties &props) : Base(props) { }
 
     std::pair<Spectrum, Mask> sample(const Scene *scene,
                                      Sampler *sampler,
-                                     const RayDifferential3f &ray,
+                                     const RayDifferential3f &ray_,
                                      const Medium *medium,
                                      Float *aovs,
                                      Mask active) const override {
         MI_MASKED_FUNCTION(ProfilerPhase::SamplingIntegratorSample, active);
 
-        PreliminaryIntersection3f pi = scene->ray_intersect_preliminary(
-            ray, /* coherent = */ true, active);
+        // Loop state
+        Ray3f ray = Ray3f(ray_);
+        Spectrum throughput = 1.0f;
+        Spectrum result = 0.0f;
+        Float eta = 1.0f;
+        UInt32 depth = 0;
+
+        Mask valid_ray = !m_hide_emitters && (scene->environment() != nullptr);
+
+        // TODO: we probably don't need this or we deal with this at the very end
+        Interaction3f prev_si = dr::zeros<Interaction3f>();
+        Float prev_bsdf_pdf = 1.0f;
+        Bool prev_bsdf_delta = true;
+        BSDFContext bsdf_ctx;
+
+        struct LoopState {
+            Ray3f ray;
+            Spectrum throughput;
+            Spectrum result;
+            Float eta;
+            UInt32 depth;
+            Mask valid_ray;
+            Interaction3f prev_si;
+            Float prev_bsdf_pdf;
+            Bool prev_bsdf_delta;
+            Bool active;
+            Sampler* sampler;
+
+            DRJIT_STRUCT(LoopState, ray, throughput, result, eta, depth, \
+                valid_ray, prev_si, prev_bsdf_pdf, prev_bsdf_delta,
+                active, sampler)
+        } ls = {
+            ray,
+            throughput,
+            result,
+            eta,
+            depth,
+            valid_ray,
+            prev_si,
+            prev_bsdf_pdf,
+            prev_bsdf_delta,
+            active,
+            sampler
+        };
+
+        // TODO: Get the right sample for the bending of rays
+        dr::tie(ls) = dr::while_loop(dr::make_tuple(ls),
+            [](const LoopState& ls) { return ls.active; },
+            [this, scene, bsdf_ctx](LoopState& ls) {
+                // TODO: for now, implement the intersection surface of the bending ray
+                SurfaceInteraction3f si = scene->ray_intersect(
+                    ls.ray, +RayFlags::All, ls.depth == 0u);
+
+                // Sample the background emitter using the calculated si
+                if (dr::any_or<true>(si.emitter(scene) != nullptr)) {
+                    DirectionSample3f ds(scene, si, ls.prev_si);
+                    Float em_pdf = 0.0f;
+
+                    if (dr::any_or<true>(!ls.prev_bsdf_delta)) {
+                        em_pdf = scene->pdf_emitter_direction(ls.prev_si, ds, !ls.prev_bsdf_delta);
+                    }
+
+                    // Compute weight for emitter sample from previous bounce
+                    Float mis_bsdf = 1;
+
+                    // Sample the emitter and accumulate the results
+                    ls.result = dr::fmadd(
+                        ls.throughput,
+                        ds.emitter->eval(si, ls.prev_bsdf_pdf > 0.0f) * mis_bsdf,
+                        ls.result);
+                } else {
+                    // If there is an intersection with the sphere, set the value manually
+                    ls.result = 0;
+                }
+
+                ls.active = false;
+            });
 
         return {
-            dr::select(pi.is_valid(), pi.t, 0.f),
-            pi.is_valid()
+            dr::select(ls.valid_ray, ls.result, 0.0f),
+            ls.valid_ray
         };
     }
 
